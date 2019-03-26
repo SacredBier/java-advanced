da5726c512567b594c8a6f4dbf4d649ba95810b0
SacredBier <sacardbier@gamil.com>
2019-03-26 09:01:11 +0800

    IO test

--- io-study/src/com/soft1841/io/FileCopy.java
+++ io-study/src/com/soft1841/io/FileCopy.java
@@ -9,7 +9,7 @@ import java.util.UUID;
  */
 
 public class FileCopy {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args){
         //指定源文件
         File srcFile = new File("D:/bg.jpg");
         //获取源文件名
@@ -23,15 +23,41 @@ public class FileCopy {
         //创建一个字节输数组，大小为源文件
         byte[] bytes = new byte[(int) srcFile.length()];
         //创建字节输入流
-        InputStream inputStream = new FileInputStream(srcFile);
+        InputStream inputStream = null;
+        try {
+            inputStream = new FileInputStream(srcFile);
+        } catch (FileNotFoundException e) {
+            System.err.println("写入出现异常");
+        }
         //将源文件读入字节数组
-        inputStream.read(bytes);
+        try {
+            inputStream.read(bytes);
+        } catch (IOException e) {
+            System.err.println("读入出现异常");
+        }
         //创建字节输出流
-        OutputStream outputStream = new FileOutputStream(destFile);
+        OutputStream outputStream = null;
+        try {
+            outputStream = new FileOutputStream(destFile);
+        } catch (FileNotFoundException e) {
+            System.err.println("创建输出出现异常");
+        }
         //将字节输出
-        outputStream.write(bytes);
+        try {
+            outputStream.write(bytes);
+        } catch (IOException e) {
+            System.err.println("字节输出出现异常");
+        }
         //关闭输入输出流
-        inputStream.close();
-        outputStream.close();
+        try {
+            inputStream.close();
+        } catch (IOException e) {
+            System.err.println("关闭输入流出现异常");
+        }
+        try {
+            outputStream.close();
+        } catch (IOException e) {
+            System.err.println("关闭输出流出现异常");
+        }
     }
 }
