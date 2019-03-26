f30cfa2dce82f2601ae41f98e7d4cbf16245d61b
SacredBier <sacardbier@gamil.com>
2019-03-26 09:52:36 +0800

    IO test

--- io-study/src/com/soft1841/io/WriterFile.java
+++ io-study/src/com/soft1841/io/WriterFile.java
@@ -1,6 +1,7 @@
 package com.soft1841.io;
 
 import java.io.*;
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -9,13 +10,21 @@ import java.util.Scanner;
  */
 public class WriterFile {
     public static void main(String[] args) throws IOException {
+        File srcFile = new File("D:/hello.txt");
+        Reader in = new FileReader(srcFile);
+        char[] srcChars = new char[(int) srcFile.length()];
+        in.read(srcChars);
+        System.out.println("文档中已有内容：");
+        System.out.println(srcChars);
+        Writer out = new FileWriter(srcFile);
         Scanner scanner = new Scanner(System.in);
         System.out.println("请输入要写入文件的内容");
         String str = scanner.nextLine();
-        char[] chars = str.toCharArray();
-        File srcFile = new File("D:/hello.txt");
-        Writer out = new FileWriter(srcFile);
+        char[] newChars = str.toCharArray();
+        char[] chars = Arrays.copyOf(srcChars, srcChars.length + newChars.length);
+        System.arraycopy(newChars, 0, chars, srcChars.length, newChars.length);
         out.write(chars);
         out.close();
+        scanner.close();
     }
 }
