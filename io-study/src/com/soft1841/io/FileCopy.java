c5fa58b87a16ad98279278e8229652d6181acb57
SacredBier <sacardbier@gamil.com>
2019-03-26 08:33:52 +0800

    IO test

---
+++ io-study/src/com/soft1841/io/FileCopy.java
@@ -0,0 +1,25 @@
+package com.soft1841.io;
+
+import java.io.*;
+
+public class FileCopy {
+    public static void main(String[] args) throws IOException{
+        //指定源文件
+        File srcFile = new File("D:/bg.jpg");
+        //指定目标文件
+        File destFile = new File("D:/测试用/bg.jpg");
+        //创建一个字节输数组，大小为源文件
+        byte[] bytes = new byte[(int) srcFile.length()];
+        //创建字节输入流
+        InputStream inputStream = new FileInputStream(srcFile);
+        //将源文件读入字节数组
+        inputStream.read(bytes);
+        //创建字节输出流
+        OutputStream outputStream = new FileOutputStream(destFile);
+        //将字节输出
+        outputStream.write(bytes);
+        //关闭输入输出流
+        inputStream.close();
+        outputStream.close();
+    }
+}
--- io-study/src/com/soft1841/io/FileDemo3.java
+++ io-study/src/com/soft1841/io/FileDemo3.java
@@ -4,12 +4,18 @@ import java.io.File;
 
 public class FileDemo3 {
     public static void main(String[] args) {
-        String[] strings = {"image", "video", "document"};
+        String[] strings = {"image", "video", "document","压缩包"};
         for (int i = 0; i < strings.length; i++) {
             File path = new File("D:/upload/" + strings[i]);
-            if (!path.exists()) {
-                path.mkdirs();
+            //当文件夹存在时删除文件夹
+            if (path.exists()){
+                path.delete();
+                new File("D:/upload").delete();
             }
+            //当文件夹不存在时创建文件夹
+//            if (!path.exists()) {
+//                path.mkdirs();
+//            }
         }
     }
 }
