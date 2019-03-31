b6a03721ef79a1170891bf7b78f5f9789d23e887
SacredBier <sacardbier@gamil.com>
2019-03-31 23:14:00 +0800

    find jpg or png

--- io-study/src/com/soft1841/io/ImgFind.java
+++ io-study/src/com/soft1841/io/ImgFind.java
@@ -4,15 +4,15 @@ import java.io.File;
 
 public class ImgFind {
     public static void main(String[] args) {
-        String path ="C:\\Users\\SacredBier\\Documents\\嵌字\\面包带来和平\\面包带来和平！第10话";
+        String path = "C:\\Users\\SacredBier\\Documents\\嵌字\\面包带来和平\\面包带来和平！第10话";
         File file = new File(path);
         File[] files = file.listFiles();
-        for (File f:files){
-            if (!f.isDirectory()){
+        for (File f : files) {
+            if (!f.isDirectory()) {
                 String srcFileName = f.getName();
                 int position = srcFileName.indexOf(".");
                 String suffixName = srcFileName.substring(position);
-                if (suffixName.equals(".jpg") | srcFileName.equals(".png")){
+                if (suffixName.equals(".jpg") | suffixName.equals(".png")) {
                     System.out.println(f);
                 }
             }
