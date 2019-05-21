56797abed159f7ec306c2841da49af01e3f2e2a4
SacredBier <sacardbier@gamil.com>
2019-05-22 06:31:26 +0800

    something..

---
+++ singleton/src/com/soft1841/Student.java
@@ -0,0 +1,24 @@
+package com.soft1841;
+
+public abstract class Student implements Study,Sport{
+    @Override
+    public void playBasketball() {
+
+    }
+
+    @Override
+    public void playFootball() {
+
+    }
+
+    @Override
+    public void javastudy() {
+
+    }
+
+    @Override
+    public void mathstudy() {
+
+    }
+}
+
